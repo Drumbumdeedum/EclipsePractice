b92a376ebeb28d8cba89b6454948c4b42cc54028
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-02 19:42:04 +0200

    practice template usage

---
+++ PracticeProject/src/main/java/dszalay/PracticeProject/test.java
@@ -0,0 +1,12 @@
+package dszalay.PracticeProject;
+
+public class test {
+	
+	public static void main(String[] args) {
+		int i = 0;
+		while(i < 5) {
+			System.out.println("This is iteration number: " + i);
+			i++;
+		}		
+	}
+}
