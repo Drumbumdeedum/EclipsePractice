5d5b8c1e471bf8b721d9c50b3b546e7d72486dab
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-06 17:48:20 +0200

    further practice

--- PracticeProject/src/main/java/dszalay/PracticeProject/test.java
+++ PracticeProject/src/main/java/dszalay/PracticeProject/test.java
@@ -2,11 +2,26 @@ package dszalay.PracticeProject;
 
 public class test {
 	
+	private static int sum;
+
 	public static void main(String[] args) {
 		int i = 0;
 		while(i < 5) {
 			System.out.println("This is iteration number: " + i);
 			i++;
-		}		
+		}	
+		setSum(0);
+		int[] numbers = {1,2,3,4,5};
+		for (int number : numbers) {
+			setSum(getSum() + number);
+		}
+	}
+
+	public static int getSum() {
+		return sum;
+	}
+
+	public static void setSum(int sum) {
+		test.sum = sum;
 	}
 }
